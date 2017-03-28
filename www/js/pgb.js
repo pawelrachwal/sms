bfb5e582c855108c8f20f2ea620d0eadd2bdd085
Paweł Rachwał <Pawel@MacBook-Air-Pawe.local>
2017-03-28 08:14:42 +0200

    Add sms

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -9,15 +9,25 @@ function onDeviceReady() {
 	navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
 }
 
-function onSuccess(acceleration) {
-    document.getElementById("move").innerHTML = 'Acceleration X: ' + acceleration.x + '\n' +
-          'Acceleration Y: ' + acceleration.y + '\n' +
-          'Acceleration Z: ' + acceleration.z + '\n' +
-          'Timestamp: '      + acceleration.timestamp + '\n';
-}
+var app = {
+    sendSms: function() {
+        var number = document.getElementById('numberTxt').value;
+        var message = document.getElementById('messageTxt').value;
+        console.log("number=" + number + ", message= " + message);
 
-function onError() {
-    alert('onError!');
-}
+        //CONFIGURATION
+        var options = {
+            replaceLineBreaks: false, // true to replace \n by a new line, false by default
+            android: {
+                intent: 'INTENT'  // send SMS with the native android SMS messaging
+                //intent: '' // send SMS without open any other app
+            }
+        };
+
+        var success = function () { alert('Message sent successfully'); };
+        var error = function (e) { alert('Message Failed:' + e); };
+        sms.send(number, message, options, success, error);
+    }
+};
 
 
