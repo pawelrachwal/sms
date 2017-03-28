92b47879f3ab5a4bdd5c78a9a5e65c6610e6019d
Paweł Rachwał <Pawel@MacBook-Air-Pawe.local>
2017-03-28 08:56:23 +0200

    Add visual improvements

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -17,15 +17,14 @@ var app = {
 
         //CONFIGURATION
         var options = {
-            replaceLineBreaks: false, // true to replace \n by a new line, false by default
+            replaceLineBreaks: false, 
             android: {
-                intent: 'INTENT'  // send SMS with the native android SMS messaging
-                //intent: '' // send SMS without open any other app
+                intent: 'INTENT' 
             }
         };
 
-        var success = function () { alert('Message sent successfully'); };
-        var error = function (e) { alert('Message Failed:' + e); };
+        var success = function () { alert('Wiadomość wysłana poprawnie'); };
+        var error = function (e) { alert('Wystąpił błąd wysyłania:' + e); };
         sms.send(number, message, options, success, error);
     }
 };
